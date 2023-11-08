7ce949edabd73db227e1b2b47b344c5745501f1b
hackieron <138766842+hackieron@users.noreply.github.com>
2023-11-08 17:15:39 +0800

    Update UserCreate.kt

--- app/src/main/java/com/example/courseer2/UserCreate.kt
+++ app/src/main/java/com/example/courseer2/UserCreate.kt
@@ -41,7 +41,7 @@ class UserCreate : AppCompatActivity() {
         setContentView(R.layout.activity_user_create)
 
         option = findViewById(R.id.strands)
-        val strands = arrayOf("ABM", "HUMSS", "ICT", "GAS","STEMS","Arts and Design","Sports", "TVL")
+        val strands = arrayOf("ABM", "HUMSS", "ICT", "GASSSSSS","STEMS","Arts and Design","Sports", "TVL")
         option.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, strands)
 
         val submit = findViewById<Button>(R.id.submit1)
